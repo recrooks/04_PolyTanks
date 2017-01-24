4c568ac79d9c87bfb9bbbb749f09857c5a8e5876
RICS_RED\Crooks <ric.crooks@gmail.com>
2017-01-24 17:18:21 -0600

    PT04 Created base Unreal level and updated .gitignore file

---
+++ PolyTanks/Source/PolyTanks/PolyTanksGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "PolyTanksGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class POLYTANKS_API APolyTanksGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
